5ef00f026b7d38fc802a0bd73f988e38b73ca4b2
Okechukwu Bright Onwumere <brighthamza9@yahoo.com>
2025-10-16 15:18:34 +0200

    Removed Content-Type

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,9 +4,9 @@ import {getBaseUrl} from "./getBaseUrl";
 const api = axios.create({
     // baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080",
     baseURL: getBaseUrl(),
-    headers: {
+   /* headers: {
         "Content-Type": "application/json",
-    },
+    },*/
 });
 
 export default api;
